62263590156d6b961177dd6fb5008f734af63579
Xiyuan <139094910+kanniwandie@users.noreply.github.com>
2025-09-15 01:38:43 +0800

    Added function: Daily Returns

---
+++ main.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Landing Page"
+)
+#not sure if we want to rename this folder cuz idt there's a way to change the name in sidebar to not be called 'main' due to streamlit's nature
