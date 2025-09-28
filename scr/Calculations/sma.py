8580aeea45e69c1049816037d4309ce24990c7de
Pskm12E <pyiesonekhaingmin123@gmail.com>
2025-09-28 15:30:57 +0800

    For athour

--- scr/Calculations/sma.py
+++ scr/Calculations/sma.py
@@ -1,6 +1,19 @@
+#Scr/Calculations/sma.py
 import pandas as pd
+import numpy as np
 
-def compute_sma(series: pd.Series, window: int = 5):
+def compute_sma(series: pd.Series, window: int = 5) -> pd.Series:
+    # Manually compute the Simple Moving Average (SMA).
+    sma_values = []
+    for i in range(len(series)):
+        if i + 1 < window:
+            sma_values.append(np.nan)  # not enough data
+        else:
+            window_vals = series[i + 1 - window : i + 1]  # last window values
+            sma_values.append(window_vals.sum() / window)
+    return pd.Series(sma_values, index=series.index)
+
+
+# ----Velidation for SMA function----
+def valadate_sma(series: pd.Series, window: int = 5):
     return series.rolling(window=window).mean()
-     
-
